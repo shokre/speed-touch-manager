def3963a12faf1440e3f00478b1ceae2f99b9ed0
shokre <kbrlek@gmail.com>
2014-05-14 16:25:20 +0200

    added configUPNP, network conf, addRouterIPAddress, auto_ip, dhcp_server, configInternet, internetState, changePass

--- SpeedTouchManager.php
+++ SpeedTouchManager.php
@@ -16,10 +16,14 @@ class SpeedTouchManager
 	private $pass;
 
 	const URL_WLAN = '/cgi/b/_wli_/cfg/';
+	const URL_INTF = '/cgi/b/intfs/_intf_/cfg/';
 	const URL_GAME_CREATE = '/cgi/b/games/newserv/';
 	const URL_GAMES_LIST = '/cgi/b/games/servdef/';
 	const URL_GAME_CONF = '/cgi/b/games/_servconf_/cfg/';
 	const URL_GAME_ASSIGN = '/cgi/b/games/cfg/';
+	const URL_INTERNET = '/cgi/b/is/_pppoe_/ov/';
+	const URL_INTERNET_STATE = '/cgi/b/is/';
+	const URL_CHANGE_PASS = '/cgi/b/users/cfg/changepsswd/';
 
 	const TYPE_802_11b = 0;
 	const TYPE_802_11b_legacy_g = 1;
@@ -118,8 +122,8 @@ class SpeedTouchManager
 	{
 		$d = $this->formData($url);
 		$data[2] = $d[2];
-
-		$this->fetch_url($url, $data);
+		list($header, $html) = $this->fetch_url($url, $data);
+		return $header['http_code'] == 200;
 	}
 
 	/**
@@ -133,15 +137,18 @@ class SpeedTouchManager
 		$dw = new DOMWalker($html, '//div[@class="contentcontainer"]');
 		$rows = $dw->query('//table[@class="edittable"]/tr');
 		$rez = array();
+		$bl = chr(194).chr(160);
 		foreach ($rows as $row) {
 			/** @var $row DOMElement */
 			if ($row->childNodes->length != 5)
 				continue;
 			$a = array();
-			$a['game'] = $row->childNodes->item(0)->nodeValue;
-			$a['assignment'] = $row->childNodes->item(1)->nodeValue;
+			$z = $row->childNodes->item(1)->nodeValue;
+			if ($z == $bl)
+				$z = '';
+			$a['assignment'] = $z;
 			$a['mode'] = $row->childNodes->item(2)->nodeValue;
-			$rez[] = $a;
+			$rez['game'] = $a;
 		}
 
 		return $rez;
@@ -208,6 +215,124 @@ class SpeedTouchManager
 		));
 	}
 
+	public function configUPNP($upnp = true, $extended_security = false)
+	{
+		$a = array(0 => 26, 1 => '');
+		if ($upnp)
+			$a[34] = 1;
+		if ($extended_security)
+			$a[35] = 1;
+		$this->postForm(self::URL_GAME_ASSIGN, $a);
+	}
+
+	/** local network stuff */
+	public function configLocal($ip_address = '192.168.1.254',
+								$netmask = '255.255.255.0',
+								$auto_ip = false,
+								$dhcp_server_enable = false,
+								$dhcp_pool_start = '192.168.7.65',
+								$dhcp_pool_end = '192.168.7.250',
+								$dhcp_lease_days = 1,
+								$dhcp_lease_hours = 0,
+								$dhcp_lease_mins = 0,
+								$dhcp_lease_secs = 0)
+	{
+		$d = $this->formData(self::URL_INTF);
+
+		$a = array(
+			0 => 10,
+			1 => '',
+			5 => 2,
+			2 => $d[2],
+			31 => 'LocalNetwork',
+			57 => $ip_address,
+			58 => $netmask,
+			33 => $d[33],
+			37 => $dhcp_pool_start,
+			38 => $dhcp_pool_end,
+			46 => $dhcp_lease_days,
+			47 => $dhcp_lease_hours,
+			48 => $dhcp_lease_mins,
+			49 => $dhcp_lease_secs,
+			51 => 'LAN_private',
+			56 => 1,
+		);
+
+		if ($auto_ip) $a[35] = 1;
+		if ($dhcp_server_enable) $a[56] = 1;
+
+
+		return $this->fetch_url(self::URL_INTF, $data);
+	}
+
+	public function addRouterIPAddress($ip_address = '192.168.1.254',
+									   $netmask = '255.255.255.0')
+	{
+		return $this->postForm(self::URL_INTF, array(
+			0 => 19,
+			1 => '',
+			5 => 0,
+			31 => 'LocalNetwork',
+			56 => 1,
+			57 => $ip_address,
+			58 => $netmask
+		));
+	}
+
+	public function services($auto_ip = false,
+							 $dhcp_server_enable = false)
+	{
+		if ($auto_ip) $a[35] = 1;
+		if ($dhcp_server_enable) $a[56] = 1;
+
+		return $this->postForm(self::URL_INTF, array(
+			0 => 10,
+			1 => '',
+			5 => 0,
+			31 => 'LocalNetwork',
+			57 => '',
+			58 => '',
+		));
+	}
+
+	/** internet conectivity */
+	public function configInternet($username, $password, $remember_pass = true)
+	{
+		$a = array(
+			0 => 12,
+			1 => 'Internet',
+			5 => '1',
+			30 => $username,
+			31 => $password,
+		);
+		if ($remember_pass)
+			$a[32] = 1;
+		return $this->postForm(self::URL_INTERNET, $a);
+	}
+
+	public function internetState($connected = false)
+	{
+		return $this->postForm(self::URL_INTERNET_STATE, array(
+			0 => $connected ? 12 : 13,
+			1 => 'Internet',
+			5 => '1',
+		));
+	}
+
+	/** current user password change */
+	public function changePass($old_pass, $new_pass)
+	{
+		return $this->postForm(self::URL_CHANGE_PASS, array(
+			0 => 10,
+			1 => 'psswApply',
+			42 => 'popup',
+			37 => $old_pass,
+			38 => $new_pass,
+			39 => $new_pass
+		));
+	}
+
+
 	public function configWLAN($enabled, $ssid, $type, $channel, $allow_multicast, $broadcast_name, $allow, $encryption, $wpa_psk, $wpa_ver)
 	{
 		$d = $this->formData(self::URL_WLAN);
@@ -235,6 +360,6 @@ class SpeedTouchManager
 
 		ksort($d);
 
-		$this->fetch_url(self::URL_WLAN, $d);
+		return $this->fetch_url(self::URL_WLAN, $d);
 	}
 }
